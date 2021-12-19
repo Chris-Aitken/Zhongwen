5ddd847d2b200d8b975b86078933d96ddd745bac
Chris-Aitken <christopher.f.aitken@gmail.com>
2021-12-19 16:39:42 +0000

    add button to create basic reactivity

--- App/app.R
+++ App/app.R
@@ -12,25 +12,45 @@ path_to_project <- "~/Documents/Personal/Chinese/Zhongwen"
 vocab <- read_excel(glue("{path_to_project}/Data/vocabulary.xlsx"))
 
 # function for getting a random entry from voab list
-get_random_entry <- function(df) {
+get_random_entry <- function(df, col) {
   df %>%
-    sample_n(1)
+    sample_n(1) %>%
+    pull({{ col }})
 }
 
 # user interface
 ui <- fluidPage(
-  tableOutput("vocab_to_test"),
+  fluidRow(
+    column(
+      4,
+      textOutput("vocab_test_prompt"),
+      actionButton("get_new_question", "Generate!"),
+      offset = 4
+    )
+  ),
   dataTableOutput("vocab_table")
 )
 
 # backend server logic
 server <- function(input, output, session) {
-  output$vocab_to_test <- renderTable(get_random_entry(vocab))
+  
+  # generate new test question
+  test_question <- reactive({
+                     input$get_new_question
+                     get_random_entry(vocab, mandarin)
+                   })
+  
+  # output test prompt
+  output$vocab_test_prompt <- renderText(test_question())
+  
+  # full vocabulary
   output$vocab_table <- renderDataTable(
                           select(vocab, -lesson),
                           options = list(pageLength = 10)
                         )
+  
 }
 
 # run it
 shinyApp(ui, server)
+
